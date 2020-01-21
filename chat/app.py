8182087be4fcba04c074a32f19b1323163109fce
diegchav <diegchav.me@gmail.com>
2020-01-20 23:32:29 -0600

    Implement login and store user id in session on successful log in to validate if user can chat

--- chat/app.py
+++ chat/app.py
@@ -1,7 +1,9 @@
+import functools
 import os
 from dotenv import load_dotenv
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, g, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,7 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-### Models ###
 ### Models ###
 class User(db.Model):
     __tablename__ = 'users'
@@ -30,10 +31,54 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 ### Routes ###
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        user = User.query.filter_by(id=user_id).first()
+        g.user = user
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 @app.route('/')
+@login_required
 def index():
     """Home page."""
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session.clear()
+            session['user_id'] = user.id
+
+            return redirect(url_for('index'))
+        else:
+            error = 'Invalid username or password'
+
+        flash(error)
+
+    if g.user:
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,9 +102,12 @@ def register():
                 db.session.add(new_user)
                 db.session.commit()
             
-            return redirect(url_for('index'))
+            return redirect(url_for('login'))
 
         flash(error)
+
+    if g.user:
+        return redirect(url_for('index'))
 
     return render_template('register.html')
 
