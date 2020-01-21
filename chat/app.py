a189b9d65751c9257e879745ce33585327346f3c
diegchav <diegchav.me@gmail.com>
2020-01-20 23:27:03 -0600

    Implement register form along with API endpoints to store users in db

--- chat/app.py
+++ chat/app.py
@@ -1,17 +1,67 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+### Models ###
+### Models ###
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username.strip().lower()
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+### Routes ###
 @app.route('/')
 def index():
     """Home page."""
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm-password']
+        error = None
+
+        if not username:
+            error = 'Username is required'
+        elif not password or not confirm_password:
+            error = 'Password is required'
+        elif password != confirm_password:
+            error = 'Password and Confirm Password don\'t match'
+        else:
+            existing_user = User.query.filter_by(username=username).first()
+            # Don't enumerate usernames
+            if existing_user is None:
+                new_user = User(username=username, password=generate_password_hash(password))
+                db.session.add(new_user)
+                db.session.commit()
+            
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run()
